e267e420c86018a40cf5c239dad5ceb489b79abb
UrusianX <rafaelrodriguessouza10@gmail.com>
2024-07-28 23:28:37 -0300

    Criação tela login

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let isLogin = document.getElementById('btn_login');
+let usuarios = [sessionStorage.getItem("user1"), sessionStorage.getItem("user2")];
+let senhas = [sessionStorage.getItem("senha1"), sessionStorage.getItem("senha2")];
+
+function login() {
+    console.log(usuarios)
+    let user = document.getElementById('txt_user').value;
+    let password = document.getElementById('txt_key').value;
+
+    for(let i = 0; i < usuarios.length; i++){
+        if(user.toLowerCase() == usuarios[i]){
+            if(password == senhas[0]){
+                garden();
+                return;
+            } else {
+                alert("Dados inválidos! \nTente Novamente");
+                clear();
+                return;
+            }
+        }
+    }
+    alert("Dados inválidos! \nTente Novamente");
+    clear();
+}
+function clear(){
+    document.getElementById("txt_user").value = "";
+    document.getElementById("txt_key").value = "";
+}
+function garden() {
+    setTimeout(function() {
+        clear();
+        window.open('page/pedido.html', "_self");
+      }, 800);
+}
+isLogin.addEventListener('click', login);
